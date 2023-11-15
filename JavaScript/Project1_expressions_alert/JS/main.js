8acccc6ccbe27cd6175e3699cf02c784b2623e81
bortega90 <147768691+bortega90@users.noreply.github.com>
2023-11-14 19:30:00 -0800

    Add files via upload

---
+++ JavaScript/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+window.alert("Hello, World!")
+document.write('Hellow, world!');
+
+var A = "This is a string";
+document.write(A);
+
+
+var B ="my name is Britny";
+window.alert(B)
+
+document.write("My mom says,\"Please call tonight\" and I forgot")
+
+document.write("\"Be who you ar and say what you feel," + "because those who mind don\'t matter.");
+
+var C ="concatenated " + " string"
+document.write(C);
+
+
+var Family ="The Ortegas", Dad = "Bob", Daughter = "Britny";
+document.write(Daughter);
+
+document.write(3+3);
